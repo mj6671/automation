0ba4ad27584f28e13d618bbd8750a381bbc986b2
Mahendran <98400425+mj6671@users.noreply.github.com>
2023-12-10 22:40:50 +0530

    Create simple logic to scroll right with pyautogui.py
    there is no option for scrolling left to right in pyautigui for Windows users

---
+++ simple logic to scroll right with pyautogui.py
@@ -0,0 +1,7 @@
+import pyautogui,time                                                                                                                                                             
+time.sleep(4)
+place=your value
+pyautogui.mouseDown(button='left')                                                                              
+pyautogui.move(place)                                                           
+pyautogui.mouseUp(button='left') 
+pyautigui.click('right')
